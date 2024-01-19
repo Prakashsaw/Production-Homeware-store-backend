059ca4a3ac5fe17cc126fbd026529a99e37721e2
Prakash Saw <prakash1999saw@gmail.com>
2024-01-19 15:08:40 +0530

    adding HTML code for backend URL working test...

--- server.js
+++ server.js
@@ -35,6 +35,28 @@ app.get("/", (req, res) => {
 //PORT
 const PORT = process.env.PORT || 8080;
 
+//it is a test route just to see our server is working
+app.get("/", (req, res) => {
+  return res.send(`<div style = "background:aqua;padding:100px;"><h2>Welcome to Expense Management System Backend Server URL...</h2>
+    <p>Below are the some examples of supported routes...</p>
+        <div><ul>
+            <h3>User Route / Auth Route</h3>
+            <li>Register User - /api/v1/auth/register</li>
+            <li>Login User - /api/v1/auth/login</li>
+            <li>Forgot Password - /api/v1/auth/forgot-password</li>
+            <li>Test - /api/v1/auth/test</li>
+            
+            <h3>category Route</h3>
+            <li>Category Route - /api/v1/category</li>
+
+            <h3>Products Route </h3>
+            <li>Products Route- /api/v1/product</li>
+            
+            <li>Much more...</li>
+        </ul></div>
+    </div>`);
+});
+
 //run listen
 app.listen(PORT, () => {
   console.log(
